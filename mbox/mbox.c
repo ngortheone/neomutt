a03373f686d9b601b4a516b15a1136b98397ce7d
Pietro Cerutti <gahr@gahr.ch>
2020-12-09 09:09:44 +0000

    Merge MxCheckReturns and MxCheckStatsReturns

--- mbox/mbox.c
+++ mbox/mbox.c
@@ -1752,11 +1752,11 @@ static int mmdf_msg_padding_size(struct Mailbox *m)
 /**
  * mbox_mbox_check_stats - Check the Mailbox statistics - Implements MxOps::mbox_check_stats()
  */
-static enum MxCheckStatsReturns mbox_mbox_check_stats(struct Mailbox *m, int flags)
+static enum MxCheckReturns mbox_mbox_check_stats(struct Mailbox *m, int flags)
 {
   struct stat sb = { 0 };
   if (stat(mailbox_path(m), &sb) != 0)
-    return MX_CHECK_STATS_ERROR;
+    return MX_CHECK_ERROR;
 
   bool new_or_changed;
 
@@ -1796,7 +1796,7 @@ static enum MxCheckStatsReturns mbox_mbox_check_stats(struct Mailbox *m, int fla
     m->peekonly = old_peek;
   }
 
-  return (m->has_new || m->msg_new) ? MX_CHECK_STATS_NEW_MAIL : MX_CHECK_STATS_NO_CHANGE;
+  return (m->has_new || m->msg_new) ? MX_CHECK_NEW_MAIL : MX_CHECK_NO_CHANGE;
 }
 
 // clang-format off
